127e8f7272a9ff1b56aa46ae98ace5bf510db4ef
starfruitone <starfruitworld@gmail.com>
2026-01-09 20:29:55 +0000

    fix

--- tokenizer-pro-max.c
+++ tokenizer-pro-max.c
@@ -4,6 +4,67 @@
 #include "shell.h"
 
 /**
+ * count_tokens - count number of tokens in a string
+ * @line: the input line
+ *
+ * Return: number of tokens
+ */
+int count_tokens(char *line)
+{
+	char *line_copy;
+	char *token;
+	int count = 0;
+
+	line_copy = malloc(strlen(line) + 1);
+	if (line_copy == NULL)
+		return (-1);
+	strcpy(line_copy, line);
+	token = strtok(line_copy, " ");
+	while (token != NULL)
+	{
+		count++;
+		token = strtok(NULL, " ");
+	}
+	free(line_copy);
+	return (count);
+}
+
+/**
+ * fill_tokens - fill token array with tokens
+ * @tokens: array to fill
+ * @line: the input line
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int fill_tokens(char **tokens, char *line)
+{
+	char *line_copy;
+	char *token;
+	int i = 0;
+
+	line_copy = malloc(strlen(line) + 1);
+	if (line_copy == NULL)
+		return (-1);
+	strcpy(line_copy, line);
+	token = strtok(line_copy, " ");
+	while (token != NULL)
+	{
+		tokens[i] = malloc(strlen(token) + 1);
+		if (tokens[i] == NULL)
+		{
+			free(line_copy);
+			return (-1);
+		}
+		strcpy(tokens[i], token);
+		i++;
+		token = strtok(NULL, " ");
+	}
+	tokens[i] = NULL;
+	free(line_copy);
+	return (0);
+}
+
+/**
  * tokenize - split a string into tokens
  * @line: the input line to tokenize
  *
@@ -12,54 +73,19 @@
 char **tokenize(char *line)
 {
 	char **tokens;
-	char *token;
-	char *line_copy;
-	int count = 0;
-	int i = 0;
+	int count;
 
-	line_copy = malloc(strlen(line) + 1);
-	if (line_copy == NULL)
+	count = count_tokens(line);
+	if (count == -1)
 		return (NULL);
-	strcpy(line_copy, line);
-
-	token = strtok(line_copy, " ");
-	while (token != NULL)
-	{
-		count++;
-		token = strtok(NULL, " ");
-	}
-
-	free(line_copy);
-
 	tokens = malloc((count + 1) * sizeof(char *));
 	if (tokens == NULL)
 		return (NULL);
-
-	line_copy = malloc(strlen(line) + 1);
-	if (line_copy == NULL)
+	if (fill_tokens(tokens, line) == -1)
 	{
-		free(tokens);
+		free_tokens(tokens);
 		return (NULL);
 	}
-	strcpy(line_copy, line);
-
-	token = strtok(line_copy, " ");
-	while (token != NULL)
-	{
-		tokens[i] = malloc(strlen(token) + 1);
-		if (tokens[i] == NULL)
-		{
-			free(line_copy);
-			free_tokens(tokens);
-			return (NULL);
-		}
-		strcpy(tokens[i], token);
-		i++;
-		token = strtok(NULL, " ");
-	}
-	tokens[i] = NULL;
-
-	free(line_copy);
 	return (tokens);
 }
 
